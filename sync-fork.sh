4ec21e095b855700770cb2c878f69c5888ee71fd
seantywork <seantywork@gmail.com>
2024-07-09 11:07:20 +0900

    use gh

--- sync-fork.sh
+++ sync-fork.sh
@@ -2,11 +2,7 @@
 
 STREAM_MAIN=($(cat stream_main.txt))
 
-STREAM_UP=($(cat stream_up.txt))
 
-STREAM_BRANCH=($(cat stream_branch.txt))
-
-STREAM_NAME=($(cat stream_name.txt))
 
 INDEX=0
 LENGTH=${#STREAM_MAIN[@]}
@@ -17,28 +13,8 @@ echo "length: $LENGTH"
 function do_sync(){
 
     target="$1"
-    upstream="$2"
-    branch="$3"
-    name="$4"
 
-
-    mkdir -p $name
-
-    cd $name
-
-    git clone $target .
-
-    git remote add stream-up $upstream
-
-    git fetch --all
-
-    git rebase stream-up/$branch 
-
-    git push
-
-    cd ../
-
-    rm -rf $name
+    gh repo sync "$target"
 
 }
 
@@ -46,21 +22,13 @@ for target in "${STREAM_MAIN[@]}"
 do
 
 
-    upstream="${STREAM_UP[$INDEX]}"
-    branch="${STREAM_BRANCH[$INDEX]}"
-    name="${STREAM_NAME[$INDEX]}"
-
     echo "--------------------------"
-    echo "UP: $upstream"
-    echo "MAIN: $target"
-    echo "BRANCH: $branch"
-    echo "NAME: $name"
+    echo "TARGET: $target"
     echo "--------------------------"
 
-    do_sync $target $upstream $branch $name
+    do_sync $target 
 
     INDEX=$((INDEX + 1))
-
 
 
 done
